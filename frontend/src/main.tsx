9b77ba35d84499623a1995623c68ae59c75def69
lordlol13 <murodhojaevmuzaffarhoja@gmail.com>
2026-06-15 12:31:43 +0500

    vercel

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import axios from "axios";
 import App from "./App";
 import "./index.css";
+
+// Use Vite env variable for backend URL (for Vercel deployment)
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL || "";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
